2961ba1c8320a4ab7b57a6ae0ca8346ab2b3f898
Mr01y <57960998+bambank1@users.noreply.github.com>
2022-11-01 21:12:53 +0700

    Add files via upload

---
+++ set.php
@@ -0,0 +1,18 @@
+<?php
+///////////////////////////////////////////////////////
+///////// KAGAK USAH DELET CREDIT AJG /////////////////
+///////////////////////////////////////////////////////
+
+
+
+# Masukan username akun ori/asli
+# Enter the original account username
+$youser="nyipto_nanda_";
+
+# Username akun tumbal
+# Enter the username of the account that was sacrificed
+$t_user="eks.mod";
+
+# Passwoard/sandi akun tumbal
+# enter the password of the account that was sacrificed
+$t_pass="eksmood@";
